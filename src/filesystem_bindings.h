1a6c6bc8d03d3b9d86b8349d7927ce8d971ae36b
Axel G. Garcia Krastek <git@axelgk.com>
2025-06-29 21:54:00 +0000

    Simplify filesystem module duktape registration

--- src/filesystem_bindings.h
+++ src/filesystem_bindings.h
@@ -1,38 +1,11 @@
 #ifndef FILESYSTEM_BINDINGS_H
 #define FILESYSTEM_BINDINGS_H
-
+#include "duktape.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#include "duktape.h"
-
-duk_ret_t duk_append_text_file(duk_context *ctx);
-duk_ret_t duk_rename_file(duk_context *ctx);
-duk_ret_t duk_file_exists(duk_context *ctx);
-duk_ret_t duk_copy_file(duk_context *ctx);
-duk_ret_t duk_copy_dir_files(duk_context *ctx);
-duk_ret_t duk_read_file(duk_context *ctx);
-duk_ret_t duk_read_text_file(duk_context *ctx);
-duk_ret_t duk_write_file(duk_context *ctx);
-duk_ret_t duk_write_text_file(duk_context *ctx);
-duk_ret_t duk_remove_file(duk_context *ctx);
-duk_ret_t duk_mkdir(duk_context *ctx);
-duk_ret_t duk_rmdir(duk_context *ctx);
-duk_ret_t duk_file_size(duk_context *ctx);
-duk_ret_t duk_move_file(duk_context *ctx);
-duk_ret_t duk_is_dir(duk_context *ctx);
-duk_ret_t duk_is_file(duk_context *ctx);
-duk_ret_t duk_touch_file(duk_context *ctx);
-duk_ret_t duk_get_mod_time(duk_context *ctx);
-duk_ret_t duk_list_files(duk_context *ctx);
-duk_ret_t duk_list_dirs(duk_context *ctx);
-duk_ret_t duk_copy_dir_files_pattern(duk_context *ctx);
-duk_ret_t duk_list_files_count(duk_context *ctx);
-duk_ret_t duk_list_dirs_count(duk_context *ctx);
-
+void register_filesystem_module(duk_context *ctx);
 #ifdef __cplusplus
 }
 #endif
-
 #endif // FILESYSTEM_BINDINGS_H
